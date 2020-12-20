8035b1dd822e355b1058a1d7a830e40a0b8a329a
sinisudhish <ssini875@gmail.com>
2020-12-20 11:56:58 +0530

    first commit

---
+++ app/src/main/java/com/example/loginandregistration/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.loginandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name;
+    EditText psw;
+    Button btn;
+    TextView viewregister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.username);
+        psw = (EditText) findViewById(R.id.password);
+        btn = (Button) findViewById(R.id.login);
+        viewregister = (TextView) findViewById(R.id.newuser);
+        viewregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
